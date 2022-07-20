456800900f78a173fd373064c05a1a4e411898b9
Egor Sadovnikov <vigarudr.work@gmail.com>
2022-07-20 18:22:14 +0400

    Improved work with database.

--- src/main/java/jsoupParser/DatabaseControl.java
+++ src/main/java/jsoupParser/DatabaseControl.java
@@ -1,44 +1,56 @@
 package jsoupParser;
 
+import javax.xml.crypto.Data;
 import java.sql.*;
 
 public class DatabaseControl {
-    Database db;
-    Connection connection;
-    Statement statement;
+    Database db = new Database();
 
-    DatabaseControl() throws SQLException {
-        db = new Database();
-        this.connection = DriverManager.getConnection("jdbc:sqlite:" + db.path);
-        this.statement = this.connection.createStatement();
+    public DatabaseControl() throws SQLException {
     }
 
     public void send(String url, String keyword, int matches) throws SQLException {
         String query =
-                "INSERT INTO " + db.tableName + " (URL, Keyword, KeywordMatches) " +
-                "VALUES ('" + url + "', '" + keyword + "', '" + matches + "')";
-        this.statement.executeUpdate(query);
+                "INSERT INTO " + db.tableName + " (URL, Keyword) " +
+                "VALUES ('" + url + "', '" + keyword + "')";
+        db.statement.executeUpdate(query);
     }
 
     public void updatePath(String path, String url) throws SQLException {
         String query = "UPDATE " + db.tableName +
                 " SET SavePath = '" + path +
                 "' WHERE URL = '" + url + "' AND SavePath IS NULL";
-        this.statement.executeUpdate(query);
+        db.statement.executeUpdate(query);
     }
 
     public void delete(String condition) throws SQLException {
         String query = "DELETE FROM "+ db.tableName + " WHERE " + condition;
-        this.statement.executeUpdate(query);
+        db.statement.executeUpdate(query);
     }
 
     public void clear() throws SQLException {
         String query = "DELETE FROM "+ db.tableName;
-        this.statement.executeUpdate(query);
+        db.statement.executeUpdate(query);
     }
 
     class Database {
         String tableName = "JsoupParserTable";
         String path = "database\\JsoupParser.db";
+        Connection connection;
+        Statement statement;
+
+        public Database() throws SQLException {
+            db = new Database();
+            this.connection = DriverManager.getConnection("jdbc:sqlite:" + this.path);
+            this.statement = this.connection.createStatement();
+
+            String query = "CREATE TABLE IF NOT EXISTS "+ this.tableName +" ( " +
+                    "    id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "    URL VARCHAR(100)," +
+                    "    Keyword VARCHAR(50)," +
+                    "    SavePath VARCHAR(50))";
+
+            this.statement.execute(query);
+        }
     }
 }
